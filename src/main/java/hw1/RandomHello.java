d39ea03dee2a5f8c7149975be066fce48a5b32ba
Justin Wang <wang.justin03@gmail.com>
2018-10-03 17:37:20 -0700

    created random hello

---
+++ src/main/java/hw1/RandomHello.java
@@ -0,0 +1,24 @@
+package hw1;
+import java.util.Random;
+
+/** RandomHello selects and prints a random greeting. */
+public class RandomHello {
+
+    /** Prints a random greeting to the console. */
+    public static void main(String[] argv) {
+        RandomHello randomHello = new RandomHello();
+        System.out.println(randomHello.getGreeting());
+    }
+
+    /** @return a greeting, randomly chosen from five possibilities */
+    public String getGreeting() {
+        Random randomGenerator = new Random();
+        String[] greetings = new String[5];
+        greetings[0] = "Hello World";
+        greetings[1] = "Hola Mundo";
+        greetings[2] = "Bonjour Monde";
+        greetings[3] = "Hallo Welt";
+        greetings[4] = "Ciao Mondo";
+        return greetings[randomGenerator.nextInt(5)];
+    }
+}
