0ac3ca8be2cdc880ca02b047df3209682604d00c
atran35 <atran35@attu6.cs.washington.edu>
2018-10-05 08:38:17 -0700

    Staff push of hw2

---
+++ src/test/java/hw2/ImplementationTests.java
@@ -0,0 +1,16 @@
+package hw2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * ImplementationTest is a simple test suite to test the implementation of each homework. You are
+ * not required to modify this file for Homework 2.
+ */
+@RunWith(Suite.class)
+@SuiteClasses({})
+public final class ImplementationTests {
+  // This class is a placeholder for the suite, so it has no members.
+  // The @SuiteClasses annotation lists the elements of the suite.
+}
---
+++ src/test/java/hw2/package-info.java
@@ -0,0 +1,2 @@
+/** Tests for HW2: Java and Coding to Specifications for CSE 331. */
+package hw2;
