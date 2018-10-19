fcb88b93653919792d92f632b698db826bba947f
Justin Wang <wang.justin03@gmail.com>
2018-10-18 23:36:19 -0700

    finished

--- src/test/java/hw3/ImplementationTests.java
+++ src/test/java/hw3/ImplementationTests.java
@@ -11,7 +11,25 @@ import org.junit.runners.Suite.SuiteClasses;
  * <p>For instance, unit tests for your individual methods would go here.
  */
 @RunWith(Suite.class)
-@SuiteClasses({CheckAsserts.class /* list classes here */})
+@SuiteClasses({CheckAsserts.class, GraphTest.class, GraphNodeTest.class, GraphEdgeTest.class})
 public final class ImplementationTests {
   // this class is a placeholder for the suite, so it has no members.
+    /**
+     * Checks that assertions are enabled. If they are not, an error message is printed, and the
+     * system exits.
+     */
+    public static void checkAssertsEnabled() {
+        try {
+            assert false;
+
+            // assertions are not enabled
+            System.err.println(
+                    "Java Asserts are not currently enabled. Follow homework writeup instructions to enable asserts on all JUnit Test files.");
+            System.exit(1);
+
+        } catch (AssertionError e) {
+            // do nothing
+            // assertions are enabled
+        }
+    }
 }
